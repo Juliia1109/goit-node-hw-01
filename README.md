f34a8cb34fe52bf1ca71a673535eb8237a46bd5b
Yuliia Cherkashyna <jul11091109@gmail.com>
2023-01-23 23:08:09 +0200

    Creat node.js hw 1

---
+++ README.md
@@ -0,0 +1,5 @@
+Сontact list      https://ibb.co/yyqFNfW
+Сontact id        https://ibb.co/CvtmX3S
+Adding a contact  https://ibb.co/9YJKnh3
+Delete a contact  https://ibb.co/wYbkJk3
+
---
+++ contacts.js
@@ -0,0 +1,51 @@
+const fs = require("fs").promises;
+const path = require("path");
+const shortid = require("shortid");
+
+const contactsPath = path.join(__dirname, "./db/contacts.json");
+const newContact = async (contacts) => {
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+};
+
+async function listContacts() {
+  const data = await fs.readFile(contactsPath);
+  return JSON.parse(data);
+}
+
+async function getContactById(contactId) {
+  const contacts = await listContacts();
+  return contacts.find((contact) => contact.id === contactId);
+}
+
+async function removeContact(contactId) {
+  const contacts = await listContacts();
+  const contactIndex = contacts.findIndex(
+    (contact) => contact.id === contactId
+  );
+  if (contactIndex === -1) {
+    return null;
+  }
+  const [result] = contacts.splice(contactIndex, 1);
+  await newContact(contacts);
+  return result;
+}
+
+async function addContact(name, email, phone) {
+  const contacts = await listContacts();
+  const contact = {
+    id: shortid.generate(),
+    name,
+    email,
+    phone,
+  };
+  contacts.push(contact);
+  await newContact(contacts);
+  return contact;
+}
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
